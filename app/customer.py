9bd1ab02f52054ac627bdc8646bc9dd6b1babf3a
Andrey Dementyev <dementyev.andreyy@gmail.com>
2024-10-26 18:33:18 +0300

    'Solution'

---
+++ app/customer.py
@@ -0,0 +1,46 @@
+from math import sqrt
+from app.car import Car
+from app.shop import Shop
+
+
+class Customer:
+    def __init__(self,
+                 name: str,
+                 product_cart: dict,
+                 location: list[int],
+                 money: float,
+                 car: dict) -> None:
+        self.name = name
+        self.product_cart = product_cart
+        self.location = location
+        self.money = money
+        self.car = Car(car["brand"], car["fuel_consumption"])
+
+    def distance_to(self, shop_location: list[int]) -> float:
+        return sqrt(
+            (self.location[0] - shop_location[0]) ** 2
+            + (self.location[1] - shop_location[1]) ** 2
+        )
+
+    def calculate_total_trip_cost(self,
+                                  shop: Shop,
+                                  fuel_price: float) -> float:
+        distance = self.distance_to(shop.location)
+        fuel_cost_to_road = (self.car.calculate_fuel_cost(distance, fuel_price)
+                             * 2)
+        product_cost = shop.calculate_product_cost(self.product_cart)
+        return fuel_cost_to_road + product_cost
+
+    def buy_products(self, shop: Shop, fuel_price: float) -> None:
+        total_trip_cost = self.calculate_total_trip_cost(shop, fuel_price)
+        if self.money >= total_trip_cost:
+            print(f"{self.name} rides to {shop.name}")
+            self.location = shop.location
+            shop.print_receipt(self.name, self.product_cart)
+            self.money -= total_trip_cost
+            print(f"{self.name} rides home")
+            self.location = [0, 0]
+            print(f"{self.name} now has {round(self.money, 2)} dollars\n")
+        else:
+            print(f"{self.name} doesn't have enough money "
+                  f"to make a purchase in any shop")
---
+++ app/shop.py
@@ -0,0 +1,39 @@
+import datetime
+
+
+class Shop:
+    def __init__(self, name: str, location: list[int], products: dict) -> None:
+        self.name = name
+        self.location = location
+        self.products = products
+
+    def calculate_single_product_cost(
+            self,
+            product: str,
+            amount: int
+    ) -> float:
+        return self.products.get(product) * amount
+
+    def calculate_product_cost(self, product_cart: dict) -> float:
+        total_cost = 0
+
+        for product, amount in product_cart.items():
+            total_cost += self.calculate_single_product_cost(product, amount)
+        return total_cost
+
+    def print_receipt(self, customer_name: str, product_cart: dict) -> None:
+        print(
+            f'\nDate: {datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}'
+        )
+        print(f"Thanks, {customer_name}, for your purchase!")
+        print("You have bought:")
+
+        for product, amount in product_cart.items():
+            cost = self.calculate_single_product_cost(product, amount)
+            formatted_cost = (f"{round(cost, 1)}" if cost % 1 != 0
+                              else f"{int(cost)}")
+            print(f"{amount} {product}s for {formatted_cost} dollars")
+
+        total_cost = self.calculate_product_cost(product_cart)
+        print(f"Total cost is {round(total_cost, 1)} dollars")
+        print("See you again!\n")
